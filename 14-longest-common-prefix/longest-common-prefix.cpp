4a70de59105283384d384cd743154ba8570d40a1
SheerWill007 <williambenjaminlaw007@gmail.com>
2026-02-06 23:55:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (29.39%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,27 @@
+static const bool __boost = []() {
+   cin.tie(nullptr);
+   cout.tie(nullptr);
+   return std::ios_base::sync_with_stdio(false);
+}();
+
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+        string prefix = strs[0];
+
+        for (int i = 1; i < strs.size(); i++) {
+            while (strs[i].find(prefix) != 0) {
+                prefix = prefix.substr(0, prefix.length() - 1);
+                if (prefix.empty()) return "";
+            }
+        }
+
+        return prefix;
+    }
+};
