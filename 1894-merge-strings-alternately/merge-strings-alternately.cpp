f9c0c659e52e71e17edfb79b26b7a5e7bcedce0c
SheerWill007 <williambenjaminlaw007@gmail.com>
2026-03-01 21:52:38 +0530

    Time: 4 ms (31.85%) | Memory: 8.8 MB (8.65%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        
+        string result;
+        result.reserve(n + m);   // Prevents multiple reallocations
+        
+        int i = 0;
+        while (i < n || i < m) {
+            if (i < n) result += word1[i];
+            if (i < m) result += word2[i];
+            i++;
+        }
+        
+        return result;
+    }
+};
