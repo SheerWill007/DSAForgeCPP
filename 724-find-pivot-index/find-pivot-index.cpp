98b272b584fa823f25d036fec46b33c0fb2d1a9b
SheerWill007 <williambenjaminlaw007@gmail.com>
2026-03-16 22:28:59 +0530

    Time: 0 ms (100.00%) | Memory: 35.7 MB (53.49%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.cpp
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -1,106 +1,27 @@
-#pragma GCC target("abm")
-#pragma GCC target("bmi")
-#pragma GCC target("avx2")
-#pragma GCC target("bmi2")
-#pragma GCC target("lzcnt")
-#pragma GCC target("popcnt")
-#pragma GCC target("native")
-#pragma GCC optimize("O3")
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("inline")
-#pragma GCC optimize("-fgcse")
-#pragma GCC optimize("fast-math")
-#pragma GCC optimize("-fgcse-lm")
-#pragma GCC optimize("-fipa-sra")
-#pragma GCC optimize("-ftree-pre")
-#pragma GCC optimize("-ftree-vrp")
-#pragma GCC optimize("-fpeephole2")
-#pragma GCC optimize("-ffast-math")
-#pragma GCC optimize("-fsched-spec")
-#pragma GCC optimize("unroll-loops")
-#pragma GCC optimize("-march=native")
-#pragma GCC optimize("-falign-jumps")
-#pragma GCC optimize("-falign-loops")
-#pragma GCC optimize("-falign-labels")
-#pragma GCC optimize("-fdevirtualize")
-#pragma GCC optimize("-fcaller-saves")
-#pragma GCC optimize("-fcrossjumping")
-#pragma GCC optimize("-fthread-jumps")
-#pragma GCC optimize("-funroll-loops")
-#pragma GCC optimize("-freorder-blocks")
-#pragma GCC optimize("-fschedule-insns")
-#pragma GCC optimize("inline-functions")
-#pragma GCC optimize("-ftree-tail-merge")
-#pragma GCC optimize("-fschedule-insnS2")
-#pragma GCC optimize("-fstrict-aliasing")
-#pragma GCC optimize("-falign-functions")
-#pragma GCC optimize("-fcse-follow-jumps")
-#pragma GCC optimize("-fsched-interblock")
-#pragma GCC optimize("-fpartial-inlining")
-#pragma GCC optimize("no-stack-protector")
-#pragma GCC optimize("-freorder-functions")
-#pragma GCC optimize("-findirect-inlining")
-#pragma GCC optimize("-fhoist-adjacent-loads")
-#pragma GCC optimize("-frerun-cse-after-loop")
-#pragma GCC optimize("inline-small-functions")
-#pragma GCC optimize("-finline-small-functions")
-#pragma GCC optimize("-ftree-switch-conversion")
-#pragma GCC optimize("-foptimize-sibling-calls")
-#pragma GCC optimize("-fexpensive-optimizations")
-#pragma GCC optimize("inline-functions-called-once")
-#pragma GCC optimize("-fdelete-null-Pointer-checks")
-static const bool __boost = []() {
-   cin.tie(nullptr);
-   cout.tie(nullptr);
-   return std::ios_base::sync_with_stdio(false);
-}();
-const size_t BUFFER_SIZE = 0x6fafffff;
-alignas(std::max_align_t) char buffer[BUFFER_SIZE];
-size_t buffer_pos = 0;
-void* operator new(size_t size) {
-   constexpr std::size_t alignment = alignof(std::max_align_t);
-   size_t padding = (alignment - (buffer_pos % alignment)) % alignment;
-   size_t total_size = size + padding;
-   char* aligned_ptr = &buffer[buffer_pos + padding];
-   buffer_pos += total_size;
-   return aligned_ptr;
-}
-void operator delete(void* ptr, unsigned long) {}
-void operator delete(void* ptr) {}
-void operator delete[](void* ptr) {}
-const auto __ = []() {
-   struct Leetcode {
-       static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
-   };
-   std::atexit(&Leetcode::_);
-   return 0;
-}();
+
+
 
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> left(n);
-        vector<int> right(n);
-
-        left[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            left[i] = left[i - 1] + nums[i];
+        int totalSum = 0;
+        
+        for (int num : nums) {
+            totalSum += num;
         }
 
-        right[n - 1] = nums[n - 1];
-        for (int i = n-2; i >= 0; i--) {
-            right[i] = right[i + 1] + nums[i];
-        }
+        int leftSum = 0;
 
-        for (int i = 0; i < n; i++) {
-            int leftSum = (i == 0) ? 0 : left[i - 1];
-            int rightSum = (i == n - 1) ? 0 : right[i + 1];
+        for (int i = 0; i < nums.size(); i++) {
+            int rightSum = totalSum - leftSum - nums[i];
 
             if (leftSum == rightSum) {
                 return i;
             }
+
+            leftSum += nums[i];
         }
+
         return -1;
     }
 };
