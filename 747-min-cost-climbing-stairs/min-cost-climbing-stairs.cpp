ca9ad853e6d541f6865a6f2f4154fff181a8b271
SheerWill007 <williambenjaminlaw007@gmail.com>
2026-03-29 23:25:46 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (87.53%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+
+        int prev2 = cost[0];
+        int prev1 = cost[1];
+
+        for (int i = 2; i < n; i++) {
+            int curr = cost[i] + min(prev1, prev2);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return min(prev1, prev2);
+    }
+};
