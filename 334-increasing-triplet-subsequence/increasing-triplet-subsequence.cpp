4b1802c4fdbc9386aad1bec92e97b1c09ce84baf
Aman Law <williambenjaminlaw007@gmail.com>
2026-05-04 23:46:18 +0530

    Time: 4 ms (26.46%) | Memory: 115.7 MB (33.89%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int first = INT_MAX;
+        int second = INT_MAX;
+
+        for (int num : nums) {
+            if (num <= first) {
+                first = num;  // smallest so far
+            } 
+            else if (num <= second) {
+                second = num; // second smallest
+            } 
+            else {
+                return true;  // found third > first and second
+            }
+        }
+        return false;
+    }
+};
